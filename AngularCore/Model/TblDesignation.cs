7ffcd0ab04d8702a3f09871f49a2a26e1794423d
Ankita00789 <ankitayadavcs93@gmail.com>
2022-07-08 14:57:57 +0530

    Add project files.

---
+++ AngularCore/Model/TblDesignation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace AngularCore.Model
+{
+    public class TblDesignation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(80)]
+        public string Designation { get; set; }
+    }
+}
